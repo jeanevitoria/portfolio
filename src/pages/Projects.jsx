3d5dcb421e58d18cd2708a000cec9089281c5e18
Vitória <jeanevitoria994@gmail.com>
2025-08-03 14:56:45 -0300

    feat: github and site urls added to 'AdotePet' project.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = forwardRef((props, ref) => {
         { title: 'Cursos', media: cursos, description: 'Aplicação SaaS que permite a profissionais, educadores e empresas disponibilizarem seus próprios conteúdos educacionais de forma organizada.', technologys: ['Nest.JS', 'JavaScript', 'Docker', 'PostGRE'], github: 'https://github.com/Plataforma-de-Cursos-Web', site: '' },
         { title: 'ProMonitor', media: promonitor, description: 'Sistema Web para gestão das monitorias do Departamento de Computação da UFRPE.', technologys: ['Javascript', 'Node.JS', 'Docker', 'MySQL'], github: 'https://github.com/HidraCode', site: '' },
         { title: 'CultBox', media: cultbox, description: 'Rede social voltada para eventos culturais, na qual os usuários e empresas podem anunciar a realização de eventos, bem como compartilhar suas experiências.', technologys: ['TypeScript', 'PostGRE', 'Python','Django'], github: 'https://github.com/Cult-Box', site: '' },
-        { title: 'AdotePet', media: adotepet, description: 'Sistema web de adoção de animais. Permite o cadastro de animais para adoção e promove a comunicação entre partes interessadas por meio de chat.', technologys: ['Javascript', 'MongoDB','Node.JS', 'Express'], github: 'https://github.com/Cult-Box', site: '' },
+        { title: 'AdotePet', media: adotepet, description: 'Sistema web de adoção de animais. Permite o cadastro de animais para adoção e promove a comunicação entre partes interessadas por meio de chat.', technologys: ['Javascript', 'MongoDB','Node.JS', 'Express'], github: 'https://github.com/jeanevitoria/AdotePet-FrontEnd', site: 'https://adotepet-six.vercel.app' },
     ]
 
     return (
