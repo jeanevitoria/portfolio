381eebf080c7155fe347ccd48c8de74c780b1019
Vitória <jeanevitoria994@gmail.com>
2025-10-18 18:33:52 -0300

    feat: novo projeto AMargem.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,9 +7,11 @@ import cursos from '../assets/cursos.png'
 import cultbox from '../assets/cultbox.png'
 import adotepet from '../assets/adotepet.png'
 import movingsound from '../assets/movingsound.png'
+import amargem from '../assets/amargem.png'
 
 const Projects = forwardRef((props, ref) => {
     const projects = [
+        { title: 'AMargem', media: amargem, description: 'Participei do desenvolvimento do site oficial do Coletivo À Margem, o primeiro grupo de teatro hip-hop de Pernambuco. O projeto teve como objetivo criar uma plataforma moderna, acessível e responsiva que refletisse a identidade visual e os valores sociais e culturais do grupo.', technologys: ['TypeScript', 'Next.JS','Prisma', 'PostGRE'], github: 'https://github.com/webdevmatias/a-margem-front', site: 'https://www.amargem.com.br/' },
         { title: 'AdotePet', media: adotepet, description: 'Sistema web de adoção de animais. Permite o cadastro de animais para adoção e promove a comunicação entre partes interessadas por meio de chat. Obs: versão em deploy sem suporte a websocket.', technologys: ['Javascript', 'MongoDB','Node.JS', 'Express'], github: 'https://github.com/jeanevitoria/AdotePet-FrontEnd', site: 'https://adotepet-six.vercel.app' },
         { title: 'MovingSound', media: movingsound, description: 'Sistema web que transforma uma playlist do Spotify em uma playlist no Youtube. Engloba a manipulação da API do Spotify em conjunto com web scrapping para mapear as músicas no Youtube.', technologys: ['Javascript', 'Django','Scrapping'], github: 'https://github.com/jeanevitoria/moving-sound', site: '' },        
         { title: 'MeetSpace', media: meetspace, description: 'Sistema distribuído utilizando a arquitetura de microsserviços que gerencia reservas de salas de reunião.', technologys: ['Node.js', 'NestJS', 'Docker', 'Python', 'TypeScript', 'JavaScript'], github: 'https://github.com/MeetSpaceUFRPE', site: '' },
